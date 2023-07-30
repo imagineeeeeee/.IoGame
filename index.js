092823dcd844e5106a09176537694feb656fca50
imagineeeeeee <109488236+imagineeeeeee@users.noreply.github.com>
2023-07-30 12:16:43 +0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>My .io Game</title>
+<style>
+body {
+  background-color: black;
+}
+
+canvas {
+  width: 100%;
+  height: 100%;
+}
+</style>
+</head>
+<body>
+<canvas id="canvas"></canvas>
+<script>
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+var player = {
+  x: 0,
+  y: 0,
+  width: 10,
+  height: 10,
+  speed: 5
+};
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "white";
+  ctx.fillRect(player.x, player.y, player.width, player.height);
+}
+
+window.addEventListener("keydown", function(event) {
+  switch(event.keyCode) {
+    case 38: // Up arrow
+      player.y -= player.speed;
+      break;
+    case 40: // Down arrow
+      player.y += player.speed;
+      break;
+    case 37: // Left arrow
+      player.x -= player.speed;
+      break;
+    case 39: // Right arrow
+      player.x += player.speed;
+      break;
+  }
+});
+
+setInterval(draw, 20);
+</script>
+</body>
+</html>
